307690a0ead4df5f3f5769a35f14f4980db49ec0
Daria <darya.funck@yandex.ru>
2025-03-05 22:16:51 +0700

    тест + рабочий код функции сдвига влево

--- main.py
+++ main.py
@@ -23,3 +23,17 @@ def empty_list(mas):
                 num = get_number_from_index(i, j)
                 list.append(num)
     return list
+
+def to_the_left(mas):
+    for row in mas:
+        while 0 in row:
+            row.remove(0)
+        while (len(row)) != 4:
+            row.append(0)
+        for i in range(4):
+            for j in range(3):
+                if mas[i][j] == mas[i][j + 1] and mas[i][j] != 0:
+                    mas[i][j] *= 2
+                    mas[i].pop(j + 1)
+                    mas[i].append(0)
+    return mas
--- tests.py
+++ tests.py
@@ -70,3 +70,36 @@ class Test_game512(unittest.TestCase):
         a = []
         self.assertEqual(empty_list(mas), a)
 
+    def test_move_left_1(self):
+        mas = [
+            [8, 8, 0, 0],
+            [2, 0, 2, 0],
+            [256, 256, 0, 0],
+            [4, 0, 0, 4],
+        ]
+
+        rez_mas = [
+            [16, 0, 0, 0],
+            [4, 0, 0, 0],
+            [512, 0, 0, 0],
+            [8, 0, 0, 0],
+        ]
+
+        self.assertEqual(to_the_left(mas), rez_mas)
+
+    def test_move_left_2(self):
+        mas = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        rez_mas = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        self.assertEqual(to_the_left(mas), rez_mas)
