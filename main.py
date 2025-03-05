d84d509ecefab8328554388bce6312581488b9ee
Daria <darya.funck@yandex.ru>
2025-03-05 21:44:40 +0700

    тест + рабочий код функции преобразования номера плитки в координаты

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 def get_number_from_index(i, j):
     return i * 4 + j + 1
+
+
+def get_index_from_number(num):
+    num -= 1
+    x, y = num // 4, num % 4
+    return x, y
--- tests.py
+++ tests.py
@@ -9,3 +9,12 @@ class Test_game512(unittest.TestCase):
 
     def test_number_from_index_2(self):
         self.assertEqual(get_number_from_index(3, 3), 16)
+
+    def test_get_index_from_number_1(self):
+        self.assertEqual(get_index_from_number(16), (3, 3))
+
+    def test_get_index_from_number_2(self):
+        self.assertEqual(get_index_from_number(1), (0, 0))
+
+    def test_get_index_from_number_3(self):
+        self.assertEqual(get_index_from_number(5), (1, 0))
